61bf57b6ccd40a6810a75935b2afa6bf7835e046
NWashington14 <nathanwashington02@gmail.com>
2023-10-07 16:52:06 -0500

    Capstone update 4 I think

--- components/views/Find_Product.js
+++ components/views/Find_Product.js
@@ -1,12 +1,6 @@
 import html from "html-literal";
 
 export default state => html`
-  <!-- HEADER -->
-  <header class="header">
-    <h1>Find Product</h1>
-    <p>You can find the product and see what is in stock.</p>
-  </header>
-
   <!-- Main -->
-  <main></main>
+  <main class="main"></main>
 `;
