be152d6f41241fb114747f40c46ad706010f9963
NWashington14 <nathanwashington02@gmail.com>
2023-10-22 19:59:15 -0500

    Capstone API update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,29 @@
 // 'Import' the Express module instead of http
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import products from "./routers/products.js";
 // Initialize the Express application
 const app = express();
+
+// Load environment variables from .env file
+dotenv.config();
+
+// mongoose connection
+mongoose.connect(process.env.MONGO_CONNECTION, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+// Mongoose middleware config
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 const PORT = process.env.PORT || 4040;
 
@@ -40,6 +62,8 @@ app.get("/status", (request, response) => {
   // End and return the response
   response.status(200).json({ message: "Service Healthy" });
 });
+
+app.use("/products", products);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
--- server/models/Product.js
+++ server/models/Product.js
@@ -1,87 +1,26 @@
 import mongoose from "mongoose";
 
-{
-  "data": [
-    {
-      "productId": "0001111041600",
-      "upc": "0001111041600",
-      "brand": "Kroger",
-      "categories": ["Dairy"],
-      "description": "Kroger 2% Reduced Fat Milk",
-      "items": [
-        {
-          "itemId": "0001111041600",
-          "inventory": {
-            "stockLevel": "HIGH"
-          },
-          "favorite": false,
-          "fulfillment": {
-            "curbside": true,
-            "delivery": true
-          },
-          "price": {
-            "regular": 1.49,
-            "promo": 0
-          },
-          "size": "1/2 Gallon"
-        }
-      ],
-      "temperature": {
-        "indicator": "Refrigerated",
-        "heatSensitive": false
-      },
-      "images": [
-        {
-          "perspective": "front",
-          "featured": true,
-          "sizes": [
-            {
-              "size": "xlarge",
-              "url": "https://www.kroger.com/product/images/xlarge/front/0001111041600"
-            },
-            {
-              "size": "small",
-              "url": "https://www.kroger.com/product/images/small/front/0001111041600"
-            },
-            {
-              "size": "large",
-              "url": "https://www.kroger.com/product/images/large/front/0001111041600"
-            },
-            {
-              "size": "medium",
-              "url": "https://www.kroger.com/product/images/medium/front/0001111041600"
-            },
-            {
-              "size": "thumbnail",
-              "url": "https://www.kroger.com/product/images/thumbnail/front/0001111041600"
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    validate: /^[A-Za-z0-9 ]*$/
+  store: { // can be used to compare item prices between stores
+    type: String, // Kroger, Walmart, etc.
+    required: true
   },
-  brand: {
-    type: String,
-    required: true,
-    validate: /^[A-Za-z0-9 ]*$/
+  brand: { // Not completely necessary but can be added if you want for further filtering
+    type: String, // Kroger, Great Value, Nestle, etc.
+    required: true
+  },
+  item: {
+    type: String, // Milk, Bread, Eggs, etc.
+    required: true
+  },
+  inventory: { // either this or inStock can be used
+    type: String, // High, Low, Out
+    required: true
   },
   price: {
-    type: Number,
+    type: String, // 1.99, 5.99, etc.
     required: true
-  },
-  inStock: {
-    type: Boolean,
-    required: true
-  },
-  category: [String]
+  }
 });
 
 const Product = mongoose.model("Product", productSchema);
